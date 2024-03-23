9545d7e3795278919de04d1f72e2d109afce7656
Stream1998 <2306120413@qq.com>
2024-03-23 22:49:11 +0800

    cute animal list

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,52 +1,198 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view>
+		<view class="menu">
+			<uni-segmented-control :current="current" :values="values" @clickItem="onClickItem" styleType="button"
+				activeColor="#4cd964"></uni-segmented-control>
+		</view>
+		<view class="animal-list">
+			<view class="animal-item" v-for="(item, index) in animals" :key="item._id" @click="preview(index)">
+				<image :src="item.url" lazy-load mode="widthFix"></image>
+				<view class="info">
+					<view class="content">{{item.content}}</view>
+					<view class="author">——{{item.author}}</view>
+				</view>
+			</view>
+		</view>
+		<view class="float">
+			<view class="item" @click="onRefresh">
+				<uni-icons type="refreshempty" size="26"></uni-icons>
+			</view>
+			<view class="item" @click="onTop">
+				<uni-icons type="arrow-up" size="26"></uni-icons>
+			</view>
+		</view>
+		<view class="loadMore">
+			<uni-load-more status="more"></uni-load-more>
 		</view>
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello Hima'
-			}
-		},
-		onLoad() {
+<script setup>
+	import {
+		computed,
+		ref
+	} from 'vue';
+	import {
+		onPullDownRefresh,
+		onLoad,
+		onReachBottom
+	} from '@dcloudio/uni-app'
 
+	const animals = ref([]);
+	const current = ref(0);
+	const menuItems = [{
+			key: 'all',
+			value: '全部'
 		},
-		methods: {
+		{
+			key: 'cat',
+			value: '猪咪'
+		},
+		{
+			key: 'dog',
+			value: '修狗'
+		},
+	]
+	const values = computed(() => menuItems.map(v => v.value));
 
-		}
+	// 修改分类
+	const onClickItem = (e) => {
+		current.value = e.currentIndex;
+		animals.value = [];
+		getAnimals()
 	}
+
+	// 获取当前动物类型
+	const getCurrentType = () => {
+		return menuItems[current.value].key;
+	}
+
+	// 获取动物列表
+	const getAnimals = () => {
+		uni.showLoading();
+		uni.showNavigationBarLoading();
+		uni.request({
+				url: "https://tea.qingnian8.com/tools/petShow",
+				data: {
+					size: 5,
+					type: getCurrentType(),
+				}
+			}).then(res => {
+				console.log(res)
+				if (res && res.data.errCode !== 0) {
+					uni.showToast({
+						title: res.errMsg,
+					})
+					return;
+				}
+				animals.value.push(...res.data.data);
+			}).catch(err => {
+				uni.showToast({
+					title: err
+				})
+			})
+			.finally(() => {
+				uni.hideLoading();
+				uni.stopPullDownRefresh();
+				uni.hideNavigationBarLoading();
+			})
+	}
+
+	// 预览图片
+	const preview = (current) => {
+		const urls = animals.value.map(v => v.url);
+		uni.previewImage({
+			current,
+			urls
+		})
+	}
+
+	// 刷新
+	const onRefresh = () => {
+		uni.startPullDownRefresh();
+	}
+
+	// 回到顶部
+	const onTop = () => {
+		uni.pageScrollTo({
+			scrollTop: 0,
+			duration: 100
+		})
+	}
+
+	onLoad(() => {
+		getAnimals();
+	})
+
+	// 下拉刷新
+	onPullDownRefresh(() => {
+		current.value = 0;
+		animals.value = [];
+		getAnimals();
+	})
+
+	// 触底加载更多
+	onReachBottom(() => {
+		getAnimals();
+	})
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+<style lang="scss">
+	.menu {
+		margin: 50rpx;
 	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
+	.animal-list {
+		.animal-item {
+			margin: 50rpx;
+			box-shadow: 2rpx 2rpx 30rpx #999;
+			border-radius: 20rpx;
+
+			image {
+				width: 100%;
+			}
+
+			.info {
+				padding: 20rpx;
+				font-size: 32rpx;
+				background-color: rgba(255, 255, 255);
+
+				.content {
+					color: #333;
+				}
+
+				.author {
+					color: #999;
+					text-align: right;
+					margin-top: 12rpx;
+				}
+			}
+		}
 	}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
+	.float {
+		position: fixed;
+		right: 50rpx;
+		bottom: calc(env(safe-area-inset-bottom) + 50rpx);
+
+		.item {
+			width: 90rpx;
+			height: 90rpx;
+			background-color: #fff;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			border-radius: 50%;
+			border: 1rpx solid #ccc;
+
+		}
+
+		.item+.item {
+			margin-top: 20rpx;
+		}
 	}
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+	.loadMore {
+		padding: calc(env(safe-area-inset-bottom) + 50rpx);
 	}
 </style>
